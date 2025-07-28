4041488905360dfa3dc38a282f4bedb8285415e5
Minh Huy <tuminhhuy4443@gmail.com>
2025-07-28 09:14:16 +0700

    Update ProductService.java

--- src/main/java/poly/edu/service/ProductService.java
+++ src/main/java/poly/edu/service/ProductService.java
@@ -2,6 +2,8 @@ package poly.edu.service;
 
 import java.util.List;
 
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
@@ -53,4 +55,8 @@ public class ProductService {
 		productDAO.deleteById(id);
 	}
 	
+	public List<Product> searchByName(String keyword) {
+	    return productDAO.findByProductNameContainingIgnoreCase(keyword);
+	}
+
 }
