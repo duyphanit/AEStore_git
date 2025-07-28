1a242b167de2610ce94e2be23a7cd4f9c1ed4652
Minh Huy <tuminhhuy4443@gmail.com>
2025-07-28 09:15:30 +0700

    Update ProductDAO.java

--- src/main/java/poly/edu/dao/ProductDAO.java
+++ src/main/java/poly/edu/dao/ProductDAO.java
@@ -3,9 +3,13 @@ package poly.edu.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import poly.edu.entity.Product;
 
 public interface ProductDAO extends JpaRepository<Product, Long>{
 	List<Product> findByCategory_Id(Long categoryId);
+	List<Product> findByProductNameContainingIgnoreCase(String keyword);
+	Page<Product> findByCategory_Id(Long categoryId, Pageable pageable);
 }
